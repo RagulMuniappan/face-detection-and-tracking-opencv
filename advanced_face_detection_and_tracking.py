4cd31e3a4912265837c00871c7b55c7c1b635e5f
ragulmuniappan@gmail.com <ragulmuniappan@gmail.com>
2025-12-24 09:27:49 +0530

    2nd commit added comments

--- advanced_face_detection_and_tracking.py
+++ advanced_face_detection_and_tracking.py
@@ -2,15 +2,15 @@ import cv2
 
 alg = "haarcascade_frontalface_default.xml"
 
-haar_cascade = cv2.CascadeClassifier(alg)
+haar_cascade = cv2.CascadeClassifier(alg) #Loading algorithm
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0) #cam id intialization
 
 while True:
-    _,img = cam.read()
-    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    _,img = cam.read() #reading the frame FRM CAM
+    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #converting clr IMG to GRAY
 
-    face = haar_cascade.detectMultiScale(grayImg,1.3,4)
+    face = haar_cascade.detectMultiScale(grayImg,1.3,4) #getting coordinates
 
     for (x,y,w,h) in face:
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
